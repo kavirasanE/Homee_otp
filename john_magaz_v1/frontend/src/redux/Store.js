05b8152dbd6de4511102bf857e71b6424711235c
Kavirasan <139606365+kavirasanE@users.noreply.github.com>
2024-04-19 18:59:19 +0530

    Delete john_magaz_v1/frontend directory

--- john_magaz_v1/frontend/src/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import FavouriteSlice from './FavouriteSlice'
-
-export const store =configureStore({
-    reducer:{
-        favourites: FavouriteSlice,
-    },
-})
